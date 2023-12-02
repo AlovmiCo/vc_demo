e2108eeb2307985f762414a4273fcbdcae1bb67b
Basil Baiju <100567242+Basil0345@users.noreply.github.com>
2023-12-02 17:11:25 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+var cors = require('cors')
+const app = express()
+const path = require("path");
+
+app.use(cors())
+require('dotenv').config()
+
+
+app.use(express.json())    // <==== parse request body as JSON
+
+
+// --------------------------Deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api is Running...')
+    })
+}
+
+// --------------------------Deployment------------------------------
+
+const port = process.env.PORT || 5000;
+
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// --------------------------Socket IO------------------------------
+
+const io = require("socket.io")(server, {
+    pingTimeout: 6000,
+    cors: {
+        origin: "http://localhost:3000"
+    },
+})
+
+// Create a users map to keep track of users
+const users = new Map();
+
+io.on('connection', socket => {
+    console.log(`user connected: ${socket.id}`);
+    users.set(socket.id, socket.id);
+
+    // emit that user has joined as soon as someone joins
+    socket.emit('user:joined', socket.id);
+
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+    });
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    });
+});
+
+app.get('/users', (req, res) => {
+    return res.json(Array.from(users));
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
+import Peer from 'simple-peer';
+
+const ENDPOINT = "http://localhost:5000";
+const socket = io(ENDPOINT);
+
+const App = () => {
+
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+  const [stream, setStream] = useState(null);
+  const [me, setMe] = useState("");
+  const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+  const [idToCall, setIdToCall] = useState("");
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((currentStream) => {
+      setStream(currentStream);
+      if (myVideo.current) {
+        // do something
+        myVideo.current.srcObject = currentStream;
+      }
+    }).catch((err) => {
+      alert(err.message)
+    })
+    socket.on("user:joined", (id) => setMe(id));
+
+    //  to handle incomming call
+    socket.on("callUser", ({ from, name: callerName, signal }) => {
+      setCall({ isReceivedCall: true, from, name: callerName, signal })
+    })
+  }, [])
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    peer.on('signal', (data) => {
+      socket.emit('answerCall', { signal: data, to: call.from });
+    });
+
+    peer.on('stream', (currentStream) => {
+      if (userVideo.current) {
+        userVideo.current.srcObject = currentStream;
+      }
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
+
+  const callUser = (id, name) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    peer.on('signal', (data) => {
+      socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
+    });
+
+    peer.on("stream", (currentStream) => {
+      if (userVideo.current) {
+        userVideo.current.srcObject = currentStream;
+      }
+    })
+
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true)
+      peer.signal(signal)
+    })
+
+    connectionRef.current = peer;
+  }
+
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    connectionRef.current.destroy();
+
+    window.location.reload()
+  }
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>video chat</h1>
+      <div style={{ display: "flex" }}>
+        {/* Our own video */}
+        {stream && <div style={{ display: "flex", flexDirection: "column" }}>
+          <video autoPlay ref={myVideo} muted />
+          <h5 style={{ textAlign: "center" }}>{name} (Me)</h5>
+          <div style={{ display: "flex" }}>
+            <div style={{ flex: 1 }}>
+              <h1>Account Info</h1>
+              <input placeholder='Name' onChange={(e) => setName(e.target.value)} />
+              <p>{me}</p>
+            </div>
+            <div>
+              <h1 style={{ backgroundColor: "green" }}>Make a call</h1>
+              <input placeholder='ID to Call' onChange={(e) => setIdToCall(e.target.value)} />
+              {callAccepted && !callEnded ? (
+                <button onClick={leaveCall}>Hang Up</button>
+              ) : (
+                <button onClick={() => { callUser(idToCall, name) }}>Make Call</button>
+
+              )}
+            </div>
+          </div>
+          {call.isReceivedCall && !callAccepted && (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <h1>{call.name} is calling...</h1>
+              <button onClick={answerCall}> Answer Call</button>
+            </div>
+          )}
+
+        </div>}
+        {/* User's video */}
+        {callAccepted && !callEnded &&
+          <> <h5>{call.name}</h5>
+            <video autoPlay ref={userVideo} muted />
+          </>
+        }
+
+
+      </div>
+    </div>
+  )
+}
+
+export default App
