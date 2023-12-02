9f7f1bfde95341f0a5bbc0dfc791e06080b08bbc
AlovmiCo <152692234+AlovmiCo@users.noreply.github.com>
2023-12-02 17:57:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const server = app.listen(port, () => {
 const io = require("socket.io")(server, {
     pingTimeout: 6000,
     cors: {
-        origin: "https://vc-mern-demo.onrender.com/"
+        origin: "https://vc-mern-demo.onrender.com"
     },
 })
 
